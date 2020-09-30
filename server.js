e8739791cc02305be271cf23cb0e96f23643eb5b
Priscila Primon <priscilaprimon@ELMAC-000160.local>
2020-09-30 18:07:15 -0300

    fix cors

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-const app = require('./src/config/custom-express');
-const cors = require('cors');
+const app = require("./src/config/custom-express");
 
-app.use((req, res, next) => {
-    console.log('Mid ok');
-   // res.header("Acess-Control-Allow-Origin", "*");
-    //app.use(cors());
-    //next();
+app.listen(3800, function () {
+  console.log("Servidor rodando na porta 3800");
 });
-
-app.listen(3800, function(){
-    console.log('Servidor rodando na porta 3800');
-})
-
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,18 +1,28 @@
-require('marko/node-require').install();
-require('marko/express');
+require("marko/node-require").install();
+require("marko/express");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require ('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+const cors = require("cors");
 
-const rotas = require('../app/rotas/rotas')
+app.options("*", cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "HEAD,OPTIONS,GET,POST",
+  })
+);
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const rotas = require("../app/rotas/rotas");
 rotas(app);
 
-require('../app/controllers/authController')(app);
-require('../app/controllers/projectController')(app);
-
+require("../app/controllers/authController")(app);
+require("../app/controllers/projectController")(app);
 
 module.exports = app;
